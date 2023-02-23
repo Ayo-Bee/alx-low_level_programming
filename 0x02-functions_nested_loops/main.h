3731b5afea4f476427608227c899f3c275892a36
Ayoola Odetunde <odetundeayoola88@gmail.com>
2023-02-23 00:34:10 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,10 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN.H
+#define MAIN.H
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
 
 int _putchar(char);
 void print_alphabet(void);
@@ -14,5 +19,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
+int _putchar(char c);
 #endif
